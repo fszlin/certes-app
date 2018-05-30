aa47982ab7f3de7acd0cf3a328de73e5ab590739
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-05-30 11:54:41 -0400

    setup DI for az func

---
+++ src/Certes.Api/ServiceProviderBuilder.cs
@@ -0,0 +1,18 @@
+﻿using DependencyInjection;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace Certes.Api
+{
+    public class ServiceProviderBuilder : IServiceProviderBuilder
+    {
+        public IServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+
+            return services.BuildServiceProvider(true);
+        }
+    }
+}
