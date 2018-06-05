251c99311d4d69aeaa5decd1265864214dfd5f88
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-06-05 12:13:39 -0400

    validate aad token

--- src/Certes.Api/AadAuthorizationAttribute.cs
+++ src/Certes.Api/AadAuthorizationAttribute.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.Azure.WebJobs.Host;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,10 +14,43 @@ namespace Certes.Api
 {
     public class AadAuthenticationAttribute : FunctionInvocationFilterAttribute
     {
-        public override Task OnExecutingAsync(FunctionExecutingContext executingContext, CancellationToken cancellationToken)
+        private const string Tenant = "certesapp";
+        private const string ClientId = "acb2ad49-ba0d-478c-a23a-f6854bd3610f";
+
+        public override async Task OnExecutingAsync(
+            FunctionExecutingContext executingContext,
+            CancellationToken cancellationToken)
         {
-            // AAD bearer token validation
-            return base.OnExecutingAsync(executingContext, cancellationToken);
+            var req = executingContext.Arguments.Values.OfType<HttpRequest>().FirstOrDefault();
+            var authzHeader = req.Headers["Authorization"].FirstOrDefault();
+
+            if (authzHeader.StartsWith("Bearer ", StringComparison.Ordinal))
+            {
+                var accessToken = authzHeader.Substring("Bearer ".Length);
+                req.HttpContext.User = await ValidateJwtToken(accessToken, cancellationToken);
+            }
+
+            await base.OnExecutingAsync(executingContext, cancellationToken);
+        }
+
+        public static async Task<ClaimsPrincipal> ValidateJwtToken(string token, CancellationToken cancellationToken)
+        {
+            var endpoint = $"https://login.microsoftonline.com/{Tenant}/.well-known/openid-configuration";
+
+            var configManager = new ConfigurationManager<OpenIdConnectConfiguration>(endpoint, new OpenIdConnectConfigurationRetriever());
+            var config = await configManager.GetConfigurationAsync(cancellationToken);
+
+            var validationParameters = new TokenValidationParameters()
+            {
+                ValidAudience = ClientId,
+                ValidIssuer = config.Issuer,
+                IssuerSigningKeys = config.SigningKeys,
+                ValidateLifetime = true,
+            };
+
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+            return jwtSecurityTokenHandler.ValidateToken(token, validationParameters, out _);
         }
     }
 }
