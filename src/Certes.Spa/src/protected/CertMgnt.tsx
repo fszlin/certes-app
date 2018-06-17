de2a28d6e1acef87cdca085762327b2ce5097c22
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-06-17 00:29:34 -0400

    + certificate list layout

--- src/Certes.Spa/src/protected/CertMgnt.tsx
+++ src/Certes.Spa/src/protected/CertMgnt.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
 import Dashboard from './Dashboard';
 
@@ -11,10 +11,34 @@ export default class CertMgnt extends React.Component<{}, {}> {
 
     public render() {
         return (
-            <>
-                <p>TODO: Protected</p>
-                <Route exact={true} path="/cert/" component={Dashboard} />
-            </>
+            <div className="row mgnt-content">
+                <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+                    <div className="sidebar-sticky">
+                        <ul className="nav flex-column">
+                            <li className="nav-item">
+                                <Link to="/cert/" className="nav-link active">Dashboard</Link>
+                            </li>
+                        </ul>
+
+                        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                            <span>Certificates</span>
+                            <a className="d-flex align-items-center text-muted" href="#">
+                                +
+                            </a>
+                        </h6>
+                        <ul className="nav flex-column mb-2">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    www.example.com
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+                    <Route exact={true} path="/cert/" component={Dashboard} />
+                </div>
+            </div>
         );
     }
 }
